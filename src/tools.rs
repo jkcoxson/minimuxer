3bc9fa6a5028d4a223449a85e6b423a03bb192ba
Jackson Coxson <jkcoxson@gmail.com>
2022-09-16 12:41:13 -0600

    Add yeet app function

--- src/tools.rs
+++ src/tools.rs
@@ -4,7 +4,12 @@ use std::os::raw::c_int;
 
 use log::{error, info, trace};
 use plist_plus::Plist;
-use rusty_libimobiledevice::{idevice, services::instproxy::InstProxyClient};
+use rusty_libimobiledevice::{
+    idevice,
+    services::{afc::AfcFileMode, instproxy::InstProxyClient},
+};
+
+const PKG_PATH: &str = "PublicStaging";
 
 pub fn enable_jit(app_id: String) -> c_int {
     trace!("Getting device from muxer");
@@ -101,7 +106,7 @@ pub fn enable_jit(app_id: String) -> c_int {
         }
     }
 
-    match debug_server.set_argv(vec![bundle_path.clone(), bundle_path.clone()]) {
+    match debug_server.set_argv(vec![bundle_path.clone(), bundle_path]) {
         Ok(res) => println!("Successfully set argv: {:?}", res),
         Err(e) => {
             error!("Error setting argv: {:?}", e);
@@ -126,4 +131,83 @@ pub fn enable_jit(app_id: String) -> c_int {
     }
 
     0
+}
+
+pub fn yeet_app_afc(bundle_id: String, bytes: Vec<u8>) -> c_int {
+    trace!("Getting device from muxer");
+    let device = match idevice::get_first_device() {
+        Ok(d) => d,
+        Err(e) => {
+            error!("Unable to get device: {:?}", e);
+            return -1;
+        }
+    };
+
+    // Start an AFC client
+    let afc = match device.new_afc_client("minimuxer") {
+        Ok(afc) => afc,
+        Err(e) => {
+            error!("Error: Could not start AFC service: {:?}", e);
+            return -1;
+        }
+    };
+
+    // Check if PublicStaging exists
+    match afc.get_file_info(format!("./{}", PKG_PATH)) {
+        Ok(_) => {}
+        Err(_) => match afc.make_directory(format!("./{}", PKG_PATH)) {
+            Ok(_) => match afc.get_file_info(format!("./{}", PKG_PATH)) {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("Unable to read PublicStaging info: {:?}", e);
+                    return -1;
+                }
+            },
+            Err(e) => {
+                error!("Unable to make PublicStaging directory: {:?}", e);
+                return -1;
+            }
+        },
+    };
+    info!("Created PublicStaging directory");
+
+    // Create bundle ID folder
+    match afc.get_file_info(format!("./{}/{}", PKG_PATH, bundle_id)) {
+        Ok(_) => {}
+        Err(_) => match afc.make_directory(format!("./{}/{}", PKG_PATH, bundle_id)) {
+            Ok(_) => match afc.get_file_info(format!("./{}/{}", PKG_PATH, bundle_id)) {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("Unable to read bundle ID info: {:?}", e);
+                    return -1;
+                }
+            },
+            Err(e) => {
+                error!("Unable to make bundle ID directory: {:?}", e);
+                return -1;
+            }
+        },
+    };
+    info!("Created bundle ID directory");
+
+    // Yeet app pls
+    let handle = match afc.file_open(
+        format!("./{}/{}/app.ipa", PKG_PATH, bundle_id),
+        AfcFileMode::WriteOnly,
+    ) {
+        Ok(h) => h,
+        Err(e) => {
+            println!("Unable to open file on device: {:?}", e);
+            return -1;
+        }
+    };
+
+    println!("Sending bytes of ipa");
+    match afc.file_write(handle, bytes) {
+        Ok(_) => 0,
+        Err(e) => {
+            println!("Unable to write ipa: {:?}", e);
+            -1
+        }
+    }
 }
